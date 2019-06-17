df9bb129ecfd60fdd4a64bfd15b06737399dd3ed
Jamie Barton <jbarton10@email.arizona.edu>
2019-06-16 18:41:54 -0700

    HTML and CSS are near compleation app still needs work

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyB4fvKhWjnQwtd0Ti8Y4Vb4yfVs8RUFbXc",
+    authDomain: "assignment7-a113b.firebaseapp.com",
+    databaseURL: "https://assignment7-a113b.firebaseio.com",
+    projectId: "assignment7-a113b",
+    storageBucket: "",
+    messagingSenderId: "581333749406",
+    appId: "1:581333749406:web:b50f200c39d9ab26"
+  };
+  firebase.initializeApp(config);
+  var database=firebase.database();
